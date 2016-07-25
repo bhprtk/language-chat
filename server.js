bddfcc028df6e22aae8c81f737f4bc008378ab3f
pratik <lazy.monk.13@gmail.com>
2016-07-24 19:40:29 -0700

    remove database for now

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 import express from 'express';
 import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import path from 'path';
+import http from 'http';
+import SocketIO from 'socket.io';
 
-import dotenv from 'dotenv';
-dotenv.load();
+
 
 // import users from './routes/users';
 
 let PORT = process.env.PORT || 3000;
 
 let app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
 
-const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost/language-chat';
 
-mongoose.connect(MONGOURL, err => {
-  console.log(err || `MongoDB connected to ${MONGOURL}`);
-});
+
 
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static('public'));
+
 
 // app.use('/users', users);
 

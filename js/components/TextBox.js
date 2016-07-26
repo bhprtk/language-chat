2ed052b6decca49d47a9b24477cbad21a4d7af83
pratik <lazy.monk.13@gmail.com>
2016-07-25 21:49:52 -0700

    messages displaying

--- js/components/TextBox.js
+++ js/components/TextBox.js
@@ -6,27 +6,45 @@ export default class TextBox extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.state = {
+			messages: []
+		}
 		this.submitMessage = this.submitMessage.bind(this);
 	}
 
 	submitMessage(e) {
 		e.preventDefault();
-
+		this.setState({messages: [this.state.message, ...this.state.messages]});
 	}
 
 	render() {
+
+		// let messages = this.state.messages.reverse();
+
 		return (
 			<div className="container" style={styles.container}>
-				<form>
-					<fieldset className="form-group">
-						<input
-							type="text"
-							className="form-control"
-							onSubmit={this.submitMessage}
-							required />
-					</fieldset>
+				<form
+					className="form-group">
+					<input
+						className="form-control"
+						type="text"
+						placeholder="Enter a message.."
+						onChange={ e => this.setState({message: e.target.value })} />
+
+					<button
+						type="submit"
+						className="btn btn-primary-outline"
+						onClick={this.submitMessage}>
+						Send
+					</button>
+
 				</form>
 
+				<ul>
+					{this.state.messages.map((message, index) => {
+						return <li key={index}>{message}</li>
+					})}
+				</ul>
 			</div>
 		)
 	}

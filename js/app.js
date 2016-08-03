f9755a267d4292d402fec0584b61aa61a8f360d5
pratik <lazy.monk.13@gmail.com>
2016-08-02 19:56:32 -0700

    remove react router

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, hashHistory } from 'react-router';
 
 import Main from "./components/Main";
 
 
 ReactDOM.render(
-	<Router history={hashHistory}>
-    <Route path="/" component={Main}/>
-  </Router>
+	<Main />
 , document.getElementById('react'));

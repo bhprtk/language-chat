ed2f0b5ef16fc8b6e392217b6b434d321e341641
pratik <lazy.monk.13@gmail.com>
2016-08-05 21:42:48 -0700

    remove unwanted packages

--- js/components/DisplayMessage.js
+++ js/components/DisplayMessage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ScrollArea from 'react-scrollbar'
 
 export default class DisplayMessage extends React.Component {
 	constructor(props) {
@@ -12,37 +11,40 @@ export default class DisplayMessage extends React.Component {
 							<ul>
 								{this.props.messages.map((message, index) => {
 									return <div key={index} className="row">
-										<span style={styles.from}>
+										<h1>
+											<span style={styles.from}>
 
-											<If condition={message.from === this.props.userData.userName}>
-												<span style={styles.you}>
-													You <span style={styles.sentAt}>{message.sentAt}</span>
+												<If condition={message.from === this.props.userData.userName}>
+													<span style={styles.you}>
+														You <span style={styles.sentAt}>{message.sentAt}</span>
+												</span>
+											</If>
+
+											<If condition={message.from !== this.props.userData.userName}>
+												<span>
+													{message.from} <span style={styles.sentAt}>{message.sentAt}</span>
 											</span>
 										</If>
 
-										<If condition={message.from !== this.props.userData.userName}>
-											<span>
-												{message.from} <span style={styles.sentAt}>{message.sentAt}</span>
-										</span>
-									</If>
-
-								</span>
+									</span>
 
 
-								<span style={styles.message}>
-									<If condition={message.message}>
-										{message.message}
-									</If>
-									<If condition={!message.message}>
-										<If condition={this.props.userData.language === "en"}>
-											{message.enMessage}
+									<span style={styles.message}>
+										<If condition={message.message}>
+											{message.message}
 										</If>
-										<If condition={this.props.userData.language === "es"}>
-											{message.esMessage}
-										</If>
+										<If condition={!message.message}>
+											<If condition={this.props.userData.language === "en"}>
+												{message.enMessage}
+											</If>
+											<If condition={this.props.userData.language === "es"}>
+												{message.esMessage}
+											</If>
 
-									</If>
-								</span>
+										</If>
+									</span>
+
+										</h1>
 							</div>
 						})}
 					</ul>
